c88ab1acc6173801f853471fe95df4d8564af66a
Akshay Mor <akshay.morx@gmail.com>
2024-05-19 17:26:37 +1200

    initial working config

--- flake.nix
+++ flake.nix
@@ -9,20 +9,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     raspberry-pi-nix.url = "github:amorx1/raspberry-pi-nix";
+    home-manager.url = "github:nix-community/home-manager/ab5542e9dbd13d0100f8baae2bc2d68af901f4b4";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, raspberry-pi-nix }:
+  outputs = { self, nixpkgs, raspberry-pi-nix, home-manager }:
     let
       inherit (nixpkgs.lib) nixosSystem;
       basic-config = { pkgs, lib, ... }: {
+        # Bootloader
+        boot.loader.grub.enable = false;
+
+        # TimeZone
         time.timeZone = "Pacific/Auckland";
+
+        # Users
         users.users.root.initialPassword = "root";
         users.users.amor = {
           isNormalUser = true;
           extraGroups = [ "wheel" ];
           packages = with pkgs; [];
         };
-        boot.loader.grub.enable = false;
+
+        # Network
         networking = {
           hostName = "nixos";
           useDHCP = false;
@@ -34,46 +43,30 @@
             };
           };
         };
+
+        # System Packages
         environment.systemPackages = with pkgs; [
-          bluez
-          bluez-tools
           helix
-          zellij
-          git
-          alacritty
-          firefox-wayland
-          hyprpaper
-          hyprpicker
-          libsecret
-          neofetch
-          waybar
-          dunst
-          libnotify
-          unzip
-          rofi-wayland
         ];
-        xdg.portal.enable = true;
-        xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-        programs.hyprland = {
-          enable = true;
-        };
-        programs.hyprland.xwayland = {
-          enable = true;
-        };
+
+        # SSH
         services.openssh.enable =  true;
         networking.firewall.allowedTCPPorts = [ 22 ];
 
+        # Bluetooth
         services.blueman.enable = true;
+
+        # Sound
         sound.enable = true;
-        services.locate = {
-          enable = true;
-          locate = pkgs.mlocate;
-        };
+
+        # Environment Variables
         environment = {
           variables = {
             CONFIG = "/etc/nixos";
           };
         };
+
+        # Hardware
         hardware = {
           bluetooth.enable = true;
           raspberry-pi = {
@@ -98,7 +91,81 @@
       nixosConfigurations = {
         rpi-example = nixosSystem {
           system = "aarch64-linux";
-          modules = [ raspberry-pi-nix.nixosModules.raspberry-pi basic-config { raspberry-pi-nix.uboot.enable = false; } ];
+          modules = [
+            raspberry-pi-nix.nixosModules.raspberry-pi
+            basic-config
+            { raspberry-pi-nix.uboot.enable = false; }
+
+            # Home Manager
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.amor = { pkgs, ... }: {
+                home.username = "amor";
+                home.homeDirectory = "/home/amor";
+                home.packages = [
+                  # Terminal
+                  pkgs.neofetch
+                  pkgs.zsh
+                  pkgs.oh-my-zsh
+                  pkgs.zellij
+                  pkgs.alacritty
+                  pkgs.zoxide
+                  pkgs.nushell
+                  pkgs.git
+                  pkgs.jq
+                ];
+
+                programs.home-manager.enable = true;
+                pkgs.jq.enable = true;
+                pkgs.neofetch.enable = true;
+
+                # Zsh
+                programs.zsh = {
+                  enable = true;
+                  shellAliases = {
+                    cd = "z";
+                  };
+                  oh-my-zsh = {
+                    enable = true;
+                    plugins = [];
+                    theme = "af-magic";
+                  };
+                };
+
+                # Nushell
+                programs.nushell = {
+                  enable = true;
+                  shellAliases = {
+                    cd = "z";
+                  };
+                };
+
+                # Git
+                programs.git = {
+                  enable = true;
+                  userName = "Akshay Mor";
+                  userEmail = "akshay.morx@gmail.com";
+                };
+
+                # Zellij
+                programs.zellij = {
+                  enable = true;
+                  enableZshIntegration = true;
+                };
+
+                # Zoxide
+                programs.zoxide = {
+                  enable = true;
+                  enableZshIntegration = true;
+                  enableNushellIntegration = true;
+                };
+
+                home.stateVersion = "23.11";
+              };
+            }
+          ];
         };
       };
     };
